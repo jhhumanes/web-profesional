25845e25bc035c4533e3ab60fd22518d773abe4a
José Humanes Humanes <proyectos@josehumaneshumanes.com>
2022-10-21 17:45:42 +0200

    Primer commit.

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { resolve } from 'path'
+
+export default {
+  root: resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 8080,
+    hot: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        // blog: resolve(__dirname, 'src/blog.html'),
+      }
+    }
+  },
+  base: '/mi-repo/',
+}
